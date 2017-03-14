9191551a8f036bfc9801e19b85aca86b99806420
Boki Milic <bojan.milic@live.com>
2017-03-14 18:37:30 +1100

    My first commit

---
+++ nickWalter/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  nickWalter
+//
+//  Created by Boki on 14/3/17.
+//  Copyright © 2017 Boki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var helloWorld: UILabel!
+    
+    
+    @IBAction func buttonTapped1(_ sender: Any) {
+        
+    }
+    
+    @IBAction func buttonTapped2(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
